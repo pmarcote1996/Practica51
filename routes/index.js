6678694308a740738c33ad1c1316755b81a74c28
pmarcote1996 <pmarcote1996@gmail.com>
2017-06-05 13:56:53 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,11 @@ router.get('/', function(req, res, next) {
 router.get('/author', function(req, res, next) {
     res.render('author');
 });
+
 // Pagina de ayuda
 router.get('/help', function(req, res, next) {
     res.render('help');
 });
-
-// Pagina de Jugar
-router.get('random_play', function(req, res, next) {
-    res.render('/quizzes/random_play');
-});
-
 
 
 // Autoload de rutas que usen :quizId
@@ -29,19 +24,19 @@ router.param('quizId', quizController.load);
 
 
 // Definición de rutas de /quizzes
-router.get('/quizzes',                     quizController.index);
-router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/new',                 quizController.new);
-router.post('/quizzes',                    quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+router.get('/quizzes',                     		quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       		quizController.show);
+router.get('/quizzes/new',                 		quizController.new);
+router.post('/quizzes',                    		quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  		quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       		quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    		quizController.destroy);
 
-router.get('/random_play',         quizController.randomp);
-router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
+router.get('/quizzes/:quizId(\\d+)/play',  		quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', 		quizController.check);
 
-router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/randomplay',	   		quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)',	quizController.randomcheck);
 
 
 module.exports = router;
