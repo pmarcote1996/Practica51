029423d5062de901fd4cb2c4713fac383911c8cd
pmarcote1996 <pmarcote1996@gmail.com>
2017-06-06 10:55:46 +0200

    Update quiz_controller.js

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -238,26 +238,54 @@ var n=0;
 
 // GET /quizzes/randomplay/
 exports.randomp = function (req, res, next) {
-models.Quiz.findAll().then(function(quizzes){
-	nojugados= nojugados || quizzes;
-	if(n<nojugados.length){
-	res.render('quizzes/random_play', {
-	quiz : nojugados[n],
-	score : score
-});
+ if (!req.session.score){
+	 req.session.score = 0;
+	}
+    if (!req.session.questions){
+	 req.session.questions = [0];//el tenia un -1, he puesto un 0 y va. Pero lo he dejado vacío y no va... Que es este objeto y por que se inicializa asi
+	} 									
 
-}else{res.render('quizzes/random_nomore', {
-	score: score
-	});
-	score=0;
-	n=0;
-}
+    //models.Quiz.count()
+    //.then(function(count) {
 
-})
-	.catch(function(err){
-	console.log("Error:",err);
-});
+	//return
+	 models.Quiz.findAll({
+	    where: { id: { $notIn: req.session.questions } }
+	})
 
+    //})
+    .then(function(quizzes) {
+	var quizID = -1;
+
+        if (quizzes.length > 0) {
+            var random = parseInt(Math.random() * quizzes.length);
+            quizID = quizzes[random].id;
+        } else {
+	    var result = req.session.score;
+	    req.session.score = 0;
+	    req.session.questions = [0];
+            res.render('quizzes/random_nomore', {
+                score: result
+            });
+		
+        }
+
+        return models.Quiz.findById(quizID);
+
+    })
+    .then(function(quiz) {
+        if (quiz) {
+            req.session.questions.push(quiz.id);//que es push y de donde sale. Es sqrl???
+            res.render('quizzes/random_play', {
+                quiz: quiz,
+                score: req.session.score
+            });
+        }
+    })
+    .catch(function(error) {
+        req.flash('error', 'Error al cargar el Quiz: ' + error.message);	
+        next(error);
+    });
 };
 
 // GET /quizzes/randomcheck/:quizId
